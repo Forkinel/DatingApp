5cb2ce05ae269b749ee51513575f13cbd2e91cc1
2030lab <bob.score@2030lab.com>
2022-05-12 15:32:06 +0100

    quick checkin @ part57

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Extensions
 {
-    public static class ApplicationServiceExtenstions
+    public static class ApplicationServiceExtensions
     {
       public static IServiceCollection AddApplicationServices(this IServiceCollection services , IConfiguration config){
           services.AddScoped<ITokenService, TokenService>();
